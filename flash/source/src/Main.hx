5fe8f44168cc60a28a86e817081275fc46645735
Mansour Djawadi <mansurjavadi@gmail.com>
2017-04-12 22:47:26 +0430

    added sample code to main

--- flash/source/src/Main.hx
+++ flash/source/src/Main.hx
@@ -2,6 +2,7 @@ package;
 
 import com.gt.tanks.IPlayer;
 import com.gt.tanks.Player;
+import com.gt.tanks.armors.ArmorType;
 import flash.display.StageAlign;
 import flash.display.StageScaleMode;
 import flash.Lib;
@@ -17,6 +18,7 @@ class Main
 	static function main() 
 	{
 		player = new Player();
+		trace(player.get_armors().get(ArmorType.heavy).unlockLevel);
 	}
 	
 }
