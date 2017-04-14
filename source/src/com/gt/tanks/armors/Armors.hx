e0eb93c36ce3cbda659554e40375e9d510a8cd10
Mansour Djawadi <mansurjavadi@gmail.com>
2017-04-14 21:06:52 +0430

    added some itrator methods

--- source/src/com/gt/tanks/armors/Armors.hx
+++ source/src/com/gt/tanks/armors/Armors.hx
@@ -11,46 +11,80 @@ package com.gt.tanks.armors;
  * @author Mansour Djawadi
  */
 
- 
- 
-
-class Armors 
+class Armors
 {
 
-#if java
+	#if java
 	private var _list:ArrayList<Armor>;
-#elseif flash
+	#elseif flash
 	private var _list:Vector<Armor>;
-#end
+	#end
 
-
-	
-	public function new() 
+	public function new()
 	{
-#if java
-	_list = new ArrayList<Armor>();
-#elseif flash
-	_list = new Vector<Armor>();
-#end
+		#if java
+		_list = new ArrayList<Armor>();
+		#elseif flash
+		_list = new Vector<Armor>();
+		#end
 	}
-	
+
 	public function length():Int
 	{
-#if java
-	return _list.size();
-#elseif flash
-	return _list.length;
-#end
-	}
-	
-	public function push(value:Armor):Void
-	{
-#if java
-	_list.add(value);
-#elseif flash
-	_list.push(value);
-#end
+		#if java
+		return _list.size();
+		#elseif flash
+		return _list.length;
+		#end
+		return 0;
 	}
 
+	public function push(value:Armor):Void
+	{
+		#if java
+		_list.add(value);
+		#elseif flash
+		_list.push(value);
+		#end
+	}
+	
+	public function pop():Void
+	{
+		#if java
+		_list.remove(_list.size()-1);
+		#elseif flash
+		_list.pop();
+		#end
+	}
+	
+	public function set(index:Int, value:Armor):Void
+	{
+		#if java
+		_list.set(index, value);
+		#elseif flash
+		_list[index] = value;
+		#end
+	}
+
+	public function get(index:Int):Armor
+	{
+		#if java
+		return _list.get(index);
+		#elseif flash
+		return _list[index];
+		#end
+		return null;
+	}
+		
+	
+	public function indexOf(armor:Armor):Int
+	{
+		#if java
+		return _list.indexOf(armor);
+		#elseif flash
+		return _list.indexOf(armor);
+		#end
+		return -1;
+	}
 	
 }
